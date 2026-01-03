467a0d0becf9bcef7979add2edf1926ac6364435
tmcelmury <trentamcelmury@gmail.com>
2026-01-03 01:36:18 -0500

    exercise 6 - use interrupt to handle the button press, and task notification for deletion

--- src/main.c
+++ src/main.c
@@ -10,41 +10,27 @@ uint32_t* const input_reg = (uint32_t*)0x3FF4403C;
 static TaskHandle_t green_led_handle;
 static TaskHandle_t yellow_led_handle;
 static TaskHandle_t red_led_handle;
-static TaskHandle_t button_handle;
 
 static TaskHandle_t volatile next_task_handle = NULL;
 
 static BaseType_t status_green_led;
 static BaseType_t status_yellow_led;
 static BaseType_t status_red_led;
-static BaseType_t status_button;
+
+static BaseType_t notify_status;
 
 static void toggle_green(void*);
 static void toggle_yellow(void*);
 static void toggle_red(void*);
-static void button_handler(void*);
+static void button_handler(void* args);
 
-static void button_handler(void*)
+static void button_handler(void* args)
 {
-    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
-    gpio_pullup_en(GPIO_NUM_0);
-    bool previous_button_state = 1;
-    bool current_button_state = 1;
-    while (true)
-    {
-        current_button_state = gpio_get_level(GPIO_NUM_0);
-        if (previous_button_state == 1 && current_button_state == 0)
-        {
-            xTaskNotify(next_task_handle, 0, eNoAction);
-        }
-        previous_button_state = current_button_state;
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
+    xTaskNotify(next_task_handle, 0, eNoAction);
 }
 
 static void toggle_green(void*)
 {
-    BaseType_t notify_status;
     gpio_set_direction(GPIO_NUM_25, GPIO_MODE_OUTPUT);
     while (true)
     {
@@ -61,7 +47,6 @@ static void toggle_green(void*)
 
 static void toggle_yellow(void*)
 {
-    BaseType_t notify_status;
     gpio_set_direction(GPIO_NUM_26, GPIO_MODE_OUTPUT);
     while (true)
     {
@@ -78,7 +63,6 @@ static void toggle_yellow(void*)
 
 static void toggle_red(void*)
 {
-    BaseType_t notify_status;
     gpio_set_direction(GPIO_NUM_27, GPIO_MODE_OUTPUT);
     while (true)
     {
@@ -94,8 +78,15 @@ static void toggle_red(void*)
 
 void app_main()
 {
-    status_button = xTaskCreate(button_handler, "Button Handler Task", 1024, NULL, 4, &button_handle);
-    status_green_led = xTaskCreate(toggle_green, "Green LED Task", 1024, NULL, 3, &green_led_handle);
+    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
+    gpio_pullup_en(GPIO_NUM_0);
+    gpio_pulldown_dis(GPIO_NUM_0);
+    gpio_intr_enable(GPIO_NUM_0);
+    gpio_set_intr_type(GPIO_NUM_0, GPIO_INTR_NEGEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(GPIO_NUM_0, button_handler, (void*) NULL);
+    
+    status_green_led = xTaskCreate(toggle_green, "Green LED Task", 2048, NULL, 3, &green_led_handle);
     next_task_handle = green_led_handle;
     status_yellow_led = xTaskCreate(toggle_yellow, "Yellow LED Task", 1024, NULL, 2, &yellow_led_handle);
     status_red_led = xTaskCreate(toggle_red, "Red LED Task", 1024, NULL, 1, &red_led_handle);
